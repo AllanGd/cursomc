1c6987f5479123019747b6e87c43d81fb2a70c49
AllanGd <allan_gd@hotmail.com>
2022-07-31 16:35:53 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/allan/cursomc/domain/Produto.java
+++ src/main/java/com/allan/cursomc/domain/Produto.java
@@ -1,5 +1,7 @@
 package com.allan.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -15,7 +17,7 @@ public class Produto implements Serializable {
     private Integer id;
     private String nome;
     private Double preco;
-
+    @JsonBackReference
     @ManyToMany
     @JoinTable(name = "PRODUTO_CATEGORIA",
             joinColumns = @JoinColumn(name = "produto_id"),
--- src/main/java/com/allan/cursomc/service/CategoriaService.java
+++ src/main/java/com/allan/cursomc/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.allan.cursomc.service;
 
 import com.allan.cursomc.domain.Categoria;
 import com.allan.cursomc.repositories.CategoriaRepository;
+import com.allan.cursomc.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,6 @@ public class CategoriaService {
     private CategoriaRepository repo;
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! ID: "+id+", Tipo: "+Categoria.class.getName()));
     }
 }
